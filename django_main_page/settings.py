e228bbe4b6a5f21c108e70857fa0f9bcfe1f8cec
simon <pailawea@gmail.com>
2026-01-23 14:40:56 -0300

    static

--- django_main_page/settings.py
+++ django_main_page/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/public/static/'
+STATIC_ROOT = '/usr/local/lsws/Example/html/django_main_page/public/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
